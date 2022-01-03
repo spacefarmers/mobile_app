bfba09882b004aaa1cb940d3dce2194b750e050e
AJ <info@spacefarmers.io>
2022-01-03 12:09:03 +0100

    Added icons

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={styles.logo}
+        source={require('./assets/adaptive-icon.png')}
+      />
+      <Text style={styles.text}>Welcome to SpaceFarmers.io!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#023c6a',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+    marginBottom: 50,
   },
 });
