c613d10a4b16525835874e441994aaed4e908d1d
AJ <info@spacefarmers.io>
2022-01-28 02:03:27 +0100

    Improved menu

--- App.js
+++ App.js
@@ -1,39 +1,96 @@
 import * as React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView
+} from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import DashboardScreen from './screens/Dashboard';
 import BlocksScreen from './screens/Blocks';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
-import DrawerItems from './constants/DrawerItems';
-import { NativeBaseProvider } from "native-base";
+import {
+  NativeBaseProvider,
+  Pressable,
+  Image,
+  VStack,
+  Text,
+  Center,
+  HStack,
+  Divider,
+  Icon,
+ } from "native-base";
 
 const Drawer = createDrawerNavigator();
+
+const getIcon = (screenName) => {
+  switch (screenName) {
+    case "Dashboard":
+      return "view-dashboard";
+    case "Blocks":
+      return "variable-box";
+    default:
+      return undefined;
+  }
+};
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props} safeArea>
+      <VStack space="6" my="2" mx="1">
+        <Center>
+          <Image source={require('./assets/logo_light.png')} alt="SpaceFarmers.io" size={"sm"} style={{ width: 250 }} resizeMode={"contain"} />
+        </Center>
+        <VStack divider={<Divider />} space="4">
+          <VStack space="3">
+            {props.state.routeNames.map((name, index) => (
+              <Pressable
+                key={name}
+                px="5"
+                py="3"
+                rounded="md"
+                bg={
+                  index === props.state.index
+                    ? "rgba(6, 182, 212, 0.1)"
+                    : "transparent"
+                }
+                onPress={(event) => {
+                  props.navigation.navigate(name);
+                }}
+              >
+                <HStack space="7" alignItems="center">
+                  <Icon
+                    color={
+                      index === props.state.index ? "primary.500" : "gray.500"
+                    }
+                    size="5"
+                    as={<MaterialCommunityIcons name={getIcon(name)} />}
+                  />
+                  <Text
+                    fontWeight="500"
+                    color={
+                      index === props.state.index ? "primary.500" : "gray.700"
+                    }
+                  >
+                    {name}
+                  </Text>
+                </HStack>
+              </Pressable>
+            ))}
+          </VStack>
+        </VStack>
+      </VStack>
+    </DrawerContentScrollView>
+  );
+}
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-        <Drawer.Navigator 
-          drawerType="front"
-          initialRouteName="Dashboard"
-          screenOptions={{
-            activeTintColor: '#e91e63',
-            itemStyle: { marginVertical: 10 },
-          }}
-        
+        <Drawer.Navigator
+          drawerContent={(props) => <CustomDrawerContent {...props} />}
         >
-          {
-            DrawerItems.map(drawer=><Drawer.Screen 
-              key={drawer.name}
-              name={drawer.name}
-              component={
-                drawer.name==='Blocks' ? BlocksScreen 
-                  : DashboardScreen
-              }
-            />)
-          }
+          <Drawer.Screen name="Dashboard" component={DashboardScreen} />
+          <Drawer.Screen name="Blocks" component={BlocksScreen} />
         </Drawer.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
