1d28b85577269c6f735c76f160488f90e5ca0f92
AJ <info@spacefarmers.io>
2022-01-29 23:46:13 +0100

    Add farm error messages

--- components/FarmAddModal.js
+++ components/FarmAddModal.js
@@ -6,7 +6,7 @@ import {
   Button,
 } from "native-base";
 
-const initialState = { loading: false, farmId: "" };
+const initialState = { loading: false, farmId: "", error: "" };
 
 export default class FarmAddModal extends React.Component {
   constructor(props) {
@@ -19,29 +19,42 @@ export default class FarmAddModal extends React.Component {
   }
 
   async addFarm() {
-    if (this.state.farmId === "")
+    this.setState({ loading: true, error: "" });
+    if (this.state.farmId === "") {
+      this.setState({ loading: false, error: "Please fill in a launcher ID" });
       return;
-    this.setState({ loading: true });
+    }
     const response = await fetch('https://spacefarmers.io/api/farmers/' + this.state.farmId);
     if (response.status === 200) {
       await this.props.addFarm(this.state.farmId);
       this.props.setShowModal(false);
       this.setState(initialState);
+    }
+    else if (response.status === 404) {
+      this.setState({ loading: false, error: "Launcher ID not found" });
     } else {
       this.setState({ loading: false });
     }
   }
 
+  closeModal = () => {
+    this.setState(initialState);
+    this.props.setShowModal(false);
+  }
+
   render() {
     return(
-      <Modal isOpen={this.props.showModal} onClose={() => this.props.setShowModal(false)}>
+      <Modal isOpen={this.props.showModal} onClose={() => this.closeModal()}>
         <Modal.Content maxWidth="400px">
           <Modal.CloseButton />
           <Modal.Header>Add farm</Modal.Header>
           <Modal.Body>
-            <FormControl>
+            <FormControl isInvalid={this.state.error != ""}>
               <FormControl.Label>Launcher ID</FormControl.Label>
               <Input value={this.state.farmId} onChangeText={this.handleChange} />
+              <FormControl.ErrorMessage>
+                { this.state.error }
+              </FormControl.ErrorMessage>
             </FormControl>
           </Modal.Body>
           <Modal.Footer>
@@ -49,18 +62,14 @@ export default class FarmAddModal extends React.Component {
               <Button
                 variant="ghost"
                 colorScheme="blueGray"
-                onPress={() => {
-                  this.props.setShowModal(false)
-                }}
+                onPress={() => this.closeModal()}
               >
                 Cancel
               </Button>
               <Button
                 isLoading={this.state.loading}
                 colorScheme="blue"
-                onPress={() => {
-                  this.addFarm();
-                }}
+                onPress={() => this.addFarm()}
               >
                 Add
               </Button>
