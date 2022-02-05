d58744a85996b3baee270bae52c3f260137a0872
AJ <info@spacefarmers.io>
2022-02-05 23:52:46 +0100

    Refactor tab screen to component

---
+++ components/FarmDetails.js
@@ -0,0 +1,167 @@
+import React from "react";
+import { Button, Center, ScrollView, Box, Heading, Text } from "native-base";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export default class FarmDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dashboardFarms: [],
+    };
+  }
+
+  async componentDidMount() {
+    let farmsStored = await AsyncStorage.getItem("@farmIds");
+    this.setState({ dashboardFarms: JSON.parse(farmsStored) });
+  }
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevState.dashboardFarms != this.state.dashboardFarms)
+      await AsyncStorage.setItem("@farmIds", JSON.stringify(this.state.dashboardFarms));
+  }
+  
+  unique(value, index, self) {
+    return self.indexOf(value) === index;
+  };
+
+  async addToDashboard() {
+    this.setState({
+      dashboardFarms: this.state.dashboardFarms
+        .concat(this.props.farm.id)
+        .filter(this.unique.bind(this)),
+    });
+  }
+
+  async removeFromDashboard() {
+    const index = this.state.dashboardFarms.indexOf(this.props.farm.id);
+    var newIds = [...this.state.dashboardFarms];
+    newIds.splice(index, 1);
+    this.setState({ dashboardFarms: newIds });
+  }
+
+
+  render() {
+    return (
+      <ScrollView py="4">
+        <Center>
+          <Box maxW="1000" w="90%">
+            <Box
+              p="3"
+              mt="3"
+              rounded="lg"
+              overflow="hidden"
+              borderColor="coolGray.200"
+              borderWidth="1"
+              minHeight="300"
+              mb="3"
+              _dark={{
+                borderColor: "coolGray.600",
+                backgroundColor: "gray.700",
+              }}
+              _web={{
+                shadow: 2,
+                borderWidth: 0,
+              }}
+              _light={{
+                backgroundColor: "gray.50",
+              }}
+            >
+              <Text
+                pt="2"
+                _light={{
+                  color: "info.700",
+                }}
+                _dark={{
+                  color: "blue.600",
+                }}
+                fontWeight="500"
+              >
+                Launcher ID
+              </Text>
+              <Heading size="sm" numberOfLines={1} ellipsizeMode="middle">
+                {this.props.farm.id}
+              </Heading>
+
+              <Text
+                pt="2"
+                _light={{
+                  color: "info.700",
+                }}
+                _dark={{
+                  color: "blue.600",
+                }}
+                fontWeight="500"
+              >
+                Size
+              </Text>
+              <Heading size="sm">
+                {this.props.farm.attributes.tib_24h} TiB
+              </Heading>
+
+              <Text
+                pt="2"
+                _light={{
+                  color: "info.700",
+                }}
+                _dark={{
+                  color: "blue.600",
+                }}
+                fontWeight="500"
+              >
+                Points
+              </Text>
+              <Heading size="sm">
+                {this.props.farm.attributes.points_24h}
+              </Heading>
+
+              <Text
+                pt="2"
+                _light={{
+                  color: "info.700",
+                }}
+                _dark={{
+                  color: "blue.600",
+                }}
+                fontWeight="500"
+              >
+                Share
+              </Text>
+              <Heading size="sm">
+                {this.props.farm.attributes.ratio_24h}%
+              </Heading>
+
+              <Text
+                pt="2"
+                _light={{
+                  color: "info.700",
+                }}
+                _dark={{
+                  color: "blue.600",
+                }}
+                fontWeight="500"
+              >
+                Effort
+              </Text>
+              <Heading size="sm">
+                {this.props.farm.attributes.current_effort}%
+              </Heading>
+            </Box>
+          </Box>
+
+          {this.state.dashboardFarms.includes(this.props.farm.id) ? (
+            <Button
+              colorScheme="gray"
+              onPress={this.removeFromDashboard.bind(this)}
+            >
+              Remove from dashboard
+            </Button>
+          ) : (
+            <Button onPress={this.addToDashboard.bind(this)}>
+              Add to dashboard
+            </Button>
+          )}
+        </Center>
+      </ScrollView>
+    );
+  }
+}
--- screens/Farmer.js
+++ screens/Farmer.js
@@ -10,16 +10,15 @@ import {
   Icon,
 } from "native-base";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { View, useWindowDimensions } from "react-native";
 import { TabView, TabBar, SceneMap } from "react-native-tab-view";
+import FarmDetails from "../components/FarmDetails";
 
 export default function FarmScreen({ route }) {
   const { farmId } = route.params;
-  const [dashboardFarms, setDashboardFarms] = useState([]);
   const [farm, setFarm] = useState({});
   const [loading, setLoading] = useState(true);
-  const [index, setIndex] = useState(0);
+  const [index, setTabIndex] = useState(0);
   const [routes] = useState([
     { key: "details", icon: "text" },
     { key: "graphs", icon: "chart-areaspline" },
@@ -29,34 +28,9 @@ export default function FarmScreen({ route }) {
   ]);
 
   useEffect(() => {
-    setIndex(0);
-    getDashboardFarms();
+    setTabIndex(0);
     getFarm();
   }, [farmId]);
-
-  useEffect(() => {
-    async function storeFarms() {
-      await AsyncStorage.setItem("@farmIds", JSON.stringify(dashboardFarms));
-    }
-
-    storeFarms();
-  }, [dashboardFarms]);
-
-  async function getDashboardFarms() {
-    let farmsStored = await AsyncStorage.getItem("@farmIds");
-    setDashboardFarms(JSON.parse(farmsStored));
-  }
-
-  async function addToDashboard() {
-    setDashboardFarms((ids) => ids.concat(farm.id).filter(unique));
-  }
-
-  async function removeFromDashboard() {
-    const index = dashboardFarms.indexOf(farm.id);
-    var newIds = [...dashboardFarms];
-    newIds.splice(index, 1);
-    setDashboardFarms(newIds);
-  }
 
   async function getFarm() {
     setLoading(true);
@@ -68,133 +42,6 @@ export default function FarmScreen({ route }) {
     setLoading(false);
   }
 
-  const unique = (value, index, self) => {
-    return self.indexOf(value) === index;
-  };
-
-  const addFarm = async (id) => {};
-
-  const FirstRoute = () => (
-    <ScrollView py="4">
-      <Center>
-        <Box maxW="1000" w="90%">
-          <Box
-            p="3"
-            mt="3"
-            rounded="lg"
-            overflow="hidden"
-            borderColor="coolGray.200"
-            borderWidth="1"
-            minHeight="300"
-            mb="3"
-            _dark={{
-              borderColor: "coolGray.600",
-              backgroundColor: "gray.700",
-            }}
-            _web={{
-              shadow: 2,
-              borderWidth: 0,
-            }}
-            _light={{
-              backgroundColor: "gray.50",
-            }}
-          >
-            <Text
-              pt="2"
-              _light={{
-                color: "info.700",
-              }}
-              _dark={{
-                color: "blue.600",
-              }}
-              fontWeight="500"
-            >
-              Launcher ID
-            </Text>
-            <Heading size="sm" numberOfLines={1} ellipsizeMode="middle">
-              {farm.id}
-            </Heading>
-
-            <Text
-              pt="2"
-              _light={{
-                color: "info.700",
-              }}
-              _dark={{
-                color: "blue.600",
-              }}
-              fontWeight="500"
-            >
-              Size
-            </Text>
-            <Heading size="sm">{farm.attributes.tib_24h} TiB</Heading>
-
-            <Text
-              pt="2"
-              _light={{
-                color: "info.700",
-              }}
-              _dark={{
-                color: "blue.600",
-              }}
-              fontWeight="500"
-            >
-              Points
-            </Text>
-            <Heading size="sm">{farm.attributes.points_24h}</Heading>
-
-            <Text
-              pt="2"
-              _light={{
-                color: "info.700",
-              }}
-              _dark={{
-                color: "blue.600",
-              }}
-              fontWeight="500"
-            >
-              Share
-            </Text>
-            <Heading size="sm">{farm.attributes.ratio_24h}%</Heading>
-
-            <Text
-              pt="2"
-              _light={{
-                color: "info.700",
-              }}
-              _dark={{
-                color: "blue.600",
-              }}
-              fontWeight="500"
-            >
-              Effort
-            </Text>
-            <Heading size="sm">{farm.attributes.current_effort}%</Heading>
-          </Box>
-        </Box>
-
-        {dashboardFarms.includes(farm.id) ? (
-          <Button
-            colorScheme="gray"
-            onPress={() => {
-              removeFromDashboard();
-            }}
-          >
-            Remove from dashboard
-          </Button>
-        ) : (
-          <Button
-            colorScheme="darkBlue"
-            onPress={() => {
-              addToDashboard();
-            }}
-          >
-            Add to dashboard
-          </Button>
-        )}
-      </Center>
-    </ScrollView>
-  );
 
   const wipRoute = () => (
     <ScrollView py="4">
@@ -231,7 +78,7 @@ export default function FarmScreen({ route }) {
   );
 
   const renderScene = SceneMap({
-    details: FirstRoute,
+    details: () => <FarmDetails farm={farm} />,
     graphs: wipRoute,
     partials: wipRoute,
     payouts: wipRoute,
@@ -266,7 +113,7 @@ export default function FarmScreen({ route }) {
             )}
             navigationState={{ index, routes }}
             renderScene={renderScene}
-            onIndexChange={setIndex}
+            onIndexChange={setTabIndex}
             initialLayout={{ width: layout.width }}
           />
         </Box>
